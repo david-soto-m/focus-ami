ed5ef7d1bea155f1d7ecd4b73cad2019d01c6a1f
david-soto-m <davidsotomarchena@protonmail.com>
2022-03-12 00:21:13 +0100

    fix changed crate name in main

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ fn main() {
     let (tx_from_killer, rx_from_killer) = mpsc::channel();
     let join_vec = vec![
         thread::spawn(move || {
-            focus::killer(tx_from_killer, rx_to_killer);
+            focus_ami::killer(tx_from_killer, rx_to_killer);
         }),
         thread::spawn(move || {
-            focus::interact(tx_to_killer, rx_from_killer);
+            focus_ami::interact(tx_to_killer, rx_from_killer);
         }),
     ];
     join_vec.into_iter().for_each(|handle| {
